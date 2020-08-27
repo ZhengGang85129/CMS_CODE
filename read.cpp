8ea036fc791efc1f05a61037c2af58c3828dd1a3
Ken <66740894+ZhengGang85129@users.noreply.github.com>
2020-08-27 16:52:50 +0800

    #2020/08/26
    Modifying  read() block and deleting read_genparticle() and read_AK4PFJet()

--- read.cpp
+++ read.cpp
@@ -12,21 +12,12 @@
 using namespace std;
 const int kMax = 10000;
 
-void read_genparticle();
-void read_AK4PFJet();
-
 void read()
 {
-    read_genparticle();
-    read_AK4PFJet();
-}
-
-void read_genparticle()
-{
-//Branch for genparticle
     TFile *f = new TFile("/Users/chenzhenggang/Desktop/local_analysis/Ntuple.root");
     //TFile *f = new TFile("/Users/chenzhenggang/Desktop/local_analysis/Ntuple_1_event.root");
     TTree *mytree = (TTree*)f->Get("Tree");
+//Branch for genparticle
     TCanvas *c1=new TCanvas("c1","c1",1600,1600);
     int genparticle_track;
     int genparticle_charge[kMax];
@@ -41,7 +32,6 @@ void read_genparticle()
     double genparticle_theta[kMax];
     double genparticle_eta[kMax];
     double genparticle_rapidity[kMax];
-    
     mytree->SetBranchAddress("genparticle_track",&genparticle_track);
     mytree->SetBranchAddress("genparticle_charge",&genparticle_charge[genparticle_track]);
     mytree->SetBranchAddress("genparticle_pdgId",&genparticle_pdgId);
@@ -56,78 +46,22 @@ void read_genparticle()
     mytree->SetBranchAddress("genparticle_eta",&genparticle_eta[genparticle_track]);
     mytree->SetBranchAddress("genparticle_rapidity",&genparticle_rapidity[genparticle_track]);
     
-    TH1F *hgenparticle_track = new TH1F("gen_track"," track distribution ", 36, 0 , 3500);
-    TH1F *hgenparticle_charge = new TH1F("gen_charge","charge distribution", 5, -2 , 2);
-    TH1F *hgenparticle_pdgId = new TH1F("gen_pdgId","PDG_ID distribution", 11, 0 , 10);
-    TH1F *hgenparticle_px = new TH1F("gen_px","Px distribution", 81, -4000, 4000 );
-    TH1F *hgenparticle_py = new TH1F("gen_py","Py distribution", 161, -4000, 4000 );
-    TH1F *hgenparticle_pz = new TH1F("gen_pz","Pz distribution", 130, -8000, 8000 );
-    TH1F *hgenparticle_pt = new TH1F("gen_pt","Pt distribution", 21, 0, 2000 );
-    TH1F *hgenparticle_mass = new TH1F("gen_mass","Mass distribution", 31, 0., 6. );
-    TH1F *hgenparticle_mt = new TH1F("gen_mt","Transverse mass distribution", 41, 0, 4000 );
-    TH1F *hgenparticle_phi = new TH1F("gen_phi","Phi distribution", 7, -TMath::Pi(),TMath::Pi() );
-    TH1F *hgenparticle_theta = new TH1F("gen_theta","Theta distribution", 13, 0,  TMath::Pi());
-    TH1F *hgenparticle_eta = new TH1F("gen_eta","Eta distribution", 61, -30000, 30000 );
-    TH1F *hgenparticle_rapidity = new TH1F("gen_rapidity","Rapidity distribution", 41 , -20, 20 );
+    TH1D *hgenparticle_track = new TH1D("gen_track"," track distribution ", 36, 0 , 3500);
+    TH1D *hgenparticle_charge = new TH1D("gen_charge","charge distribution", 5, -2 , 2);
+    TH1D *hgenparticle_pdgId = new TH1D("gen_pdgId","PDG_ID distribution", 11, 0 , 10);
+    TH1D *hgenparticle_px = new TH1D("gen_px","Px distribution", 800, -4000, 4000 );
+    TH1D *hgenparticle_py = new TH1D("gen_py","Py distribution", 800, -4000, 4000 );
+    TH1D *hgenparticle_pz = new TH1D("gen_pz","Pz distribution", 1600, -8000, 8000 );
+    TH1D *hgenparticle_pt = new TH1D("gen_pt","Pt distribution", 200, 0, 2000 );
+    TH1D *hgenparticle_mass = new TH1D("gen_mass","Mass distribution", 31, 0., 6. );
+    TH1D *hgenparticle_mt = new TH1D("gen_mt","Transverse mass distribution", 41, 0, 4000 );
+    TH1D *hgenparticle_phi = new TH1D("gen_phi","Phi distribution", 200, -TMath::Pi(),TMath::Pi() );
+    TH1D *hgenparticle_theta = new TH1D("gen_theta","Theta distribution", 300, 0,  TMath::Pi());
+    TH1D *hgenparticle_eta = new TH1D("gen_eta","Eta distribution", 6000, -30000, 30000 );
+    TH1D *hgenparticle_rapidity = new TH1D("gen_rapidity","Rapidity distribution", 200 , -20, 20 );
 
-    int nentries = (int)mytree->GetEntries();
-
-    for( int i = 0 ; i < nentries ; i++)
-    {
-       mytree->GetEntry(i);
-       hgenparticle_track->Fill(genparticle_track);
-      for(int cnt = 0; cnt < genparticle_track ; cnt ++)
-      {
-       hgenparticle_charge->Fill(genparticle_charge[cnt]);
-       hgenparticle_pdgId->Fill(genparticle_pdgId[cnt]);
-       hgenparticle_px->Fill(genparticle_px[cnt]);
-       hgenparticle_py->Fill(genparticle_py[cnt]);
-       hgenparticle_pz->Fill(genparticle_pz[cnt]);
-       hgenparticle_pt->Fill(genparticle_pt[cnt]);
-       hgenparticle_mass->Fill(genparticle_mass[cnt]);
-       hgenparticle_mt->Fill(genparticle_mt[cnt]);
-       hgenparticle_phi->Fill(genparticle_phi[cnt]);
-       hgenparticle_theta->Fill(genparticle_theta[cnt]);
-       hgenparticle_eta->Fill(genparticle_eta[cnt]);
-       hgenparticle_rapidity->Fill(genparticle_rapidity[cnt]);
-      }
-      /*
-*/
-    }
-    hgenparticle_track->Draw();
-    c1->SaveAs("ntuple.pdf(");
-    hgenparticle_charge->Draw();
-    c1->SaveAs("ntuple.pdf");
-    hgenparticle_pdgId->Draw();
-    c1->SaveAs("ntuple.pdf");
-    hgenparticle_px->Draw();
-    c1->SaveAs("ntuple.pdf");
-    hgenparticle_py->Draw();
-    c1->SaveAs("ntuple.pdf");
-    hgenparticle_pz->Draw();
-    c1->SaveAs("ntuple.pdf");
-    hgenparticle_pt->Draw();
-    c1->SaveAs("ntuple.pdf");
-    hgenparticle_mass->Draw();
-    c1->SaveAs("ntuple.pdf");
-    hgenparticle_mt->Draw();
-    c1->SaveAs("ntuple.pdf");
-    hgenparticle_phi->Draw();
-    c1->SaveAs("ntuple.pdf");
-    hgenparticle_theta->Draw();
-    c1->SaveAs("ntuple.pdf");
-    hgenparticle_eta->Draw();
-    c1->SaveAs("ntuple.pdf");
-    hgenparticle_rapidity->Draw();
-    c1->SaveAs("ntuple.pdf)");
-}
-void read_AK4PFJet()
-{
-//Branch for ak4pfjets
-    //TFile *f = new TFile("/Users/chenzhenggang/Desktop/local_analysis/Ntuple_1_event.root");
-    TFile *f = new TFile("/Users/chenzhenggang/Desktop/local_analysis/Ntuple.root");
-    TTree *mytree = (TTree*)f->Get("Tree");
-    TCanvas *c1=new TCanvas("c1","c1",1600,1600);
+    //For AK4PFJets
+    TCanvas *c2=new TCanvas("c2","c2",1600,1600);
     
     int ak4pfjets_track;
     int ak4pfjets_charge[kMax];
@@ -157,25 +91,42 @@ void read_AK4PFJet()
     mytree->SetBranchAddress("ak4pfjets_eta",&ak4pfjets_eta[ak4pfjets_track]);
     mytree->SetBranchAddress("ak4pfjets_rapidity",&ak4pfjets_rapidity[ak4pfjets_track]);
     
-    TH1F *hak4pfjets_track = new TH1F("ak4pfjets_track"," track distribution ", 31, 0 , 30);
-    TH1F *hak4pfjets_charge = new TH1F("ak4pfjets_charge","charge distribution", 19, -8 , 10);
-    TH1F *hak4pfjets_pdgId = new TH1F("ak4pfjets_pdgId","PDG_ID distribution", 5, -2 , 2);
-    TH1F *hak4pfjets_px = new TH1F("ak4pfjets_px","Px distribution", 81, -4000, 4000 );
-    TH1F *hak4pfjets_py = new TH1F("ak4pfjets_py","Py distribution", 81, -4000, 4000 );
-    TH1F *hak4pfjets_pz = new TH1F("ak4pfjets_pz","Pz distribution", 120, -7000, 4000 );
-    TH1F *hak4pfjets_pt = new TH1F("ak4pfjets_pt","Pt distribution", 41, 0, 4000 );
-    TH1F *hak4pfjets_mass = new TH1F("ak4pfjets_mass","Mass distribution", 46, 0., 450. );
-    TH1F *hak4pfjets_mt = new TH1F("ak4pfjets_mt","Transverse mass distribution", 41, 0, 4000 );
-    TH1F *hak4pfjets_phi = new TH1F("ak4pfjets_phi","Phi distribution", 31, -TMath::Pi(),TMath::Pi() );
-    TH1F *hak4pfjets_theta = new TH1F("ak4pfjets_theta","Theta distribution", 300, 0,  TMath::Pi());
-    TH1F *hak4pfjets_eta = new TH1F("ak4pfjets_eta","Eta distribution", 201, -6., 6. );
-    TH1F *hak4pfjets_rapidity = new TH1F("ak4pfjets_rapidity","Rapidity distribution",101 , -6., 6. );
-
-    int nentries = (int)mytree->GetEntries();
+    TH1D *hak4pfjets_track = new TH1D("ak4pfjets_track"," track distribution ", 31, 0 , 30);
+    TH1D *hak4pfjets_charge = new TH1D("ak4pfjets_charge","charge distribution", 19, -8 , 10);
+    TH1D *hak4pfjets_pdgId = new TH1D("ak4pfjets_pdgId","PDG_ID distribution", 5, -2 , 2);
+    TH1D *hak4pfjets_px = new TH1D("ak4pfjets_px","Px distribution", 800, -4000, 4000 );
+    TH1D *hak4pfjets_py = new TH1D("ak4pfjets_py","Py distribution", 800, -4000, 4000 );
+    TH1D *hak4pfjets_pz = new TH1D("ak4pfjets_pz","Pz distribution", 1100, -7000, 4000 );
+    TH1D *hak4pfjets_pt = new TH1D("ak4pfjets_pt","Pt distribution", 1100, 0, 4000 );
+    TH1D *hak4pfjets_mass = new TH1D("ak4pfjets_mass","Mass distribution", 46, 0., 450. );
+    TH1D *hak4pfjets_mt = new TH1D("ak4pfjets_mt","Transverse mass distribution", 400, 0, 4000 );
+    TH1D *hak4pfjets_phi = new TH1D("ak4pfjets_phi","Phi distribution", 200, -TMath::Pi(),TMath::Pi() );
+    TH1D *hak4pfjets_theta = new TH1D("ak4pfjets_theta","Theta distribution", 300, 0,  TMath::Pi());
+    TH1D *hak4pfjets_eta = new TH1D("ak4pfjets_eta","Eta distribution", 200, -6., 6. );
+    TH1D *hak4pfjets_rapidity = new TH1D("ak4pfjets_rapidity","Rapidity distribution",200 , -6., 6. );
+    
+    Int_t nentries = (Int_t)mytree->GetEntries();
 
     for( int i = 0 ; i < nentries ; i++)
     {
        mytree->GetEntry(i);
+       
+       hgenparticle_track->Fill(genparticle_track);
+      for(int cnt = 0; cnt < genparticle_track ; cnt ++)
+      {
+       hgenparticle_charge->Fill(genparticle_charge[cnt]);
+       hgenparticle_pdgId->Fill(genparticle_pdgId[cnt]);
+       hgenparticle_px->Fill(genparticle_px[cnt]);
+       hgenparticle_py->Fill(genparticle_py[cnt]);
+       hgenparticle_pz->Fill(genparticle_pz[cnt]);
+       hgenparticle_pt->Fill(genparticle_pt[cnt]);
+       hgenparticle_mass->Fill(genparticle_mass[cnt]);
+       hgenparticle_mt->Fill(genparticle_mt[cnt]);
+       hgenparticle_phi->Fill(genparticle_phi[cnt]);
+       hgenparticle_theta->Fill(genparticle_theta[cnt]);
+       hgenparticle_eta->Fill(genparticle_eta[cnt]);
+       hgenparticle_rapidity->Fill(genparticle_rapidity[cnt]);
+      }
        hak4pfjets_track->Fill(ak4pfjets_track);
       for(int cnt = 0; cnt < ak4pfjets_track ; cnt ++)
       {
@@ -195,32 +146,62 @@ void read_AK4PFJet()
       /*
 */
     }
+    c1->cd(); 
+    hgenparticle_track->Draw();
+    c1->SaveAs("ntuple.pdf(");
+    hgenparticle_charge->Draw();
+    c1->SaveAs("ntuple.pdf");
+    hgenparticle_pdgId->Draw();
+    c1->SaveAs("ntuple.pdf");
+    hgenparticle_px->Draw();
+    c1->SaveAs("ntuple.pdf");
+    hgenparticle_py->Draw();
+    c1->SaveAs("ntuple.pdf");
+    hgenparticle_pz->Draw();
+    c1->SaveAs("ntuple.pdf");
+    hgenparticle_pt->Draw();
+    c1->SaveAs("ntuple.pdf");
+    hgenparticle_mass->Draw();
+    c1->SaveAs("ntuple.pdf");
+    hgenparticle_mt->Draw();
+    c1->SaveAs("ntuple.pdf");
+    hgenparticle_phi->Draw();
+    c1->SaveAs("ntuple.pdf");
+    hgenparticle_theta->Draw();
+    c1->SaveAs("ntuple.pdf");
+    hgenparticle_eta->Draw();
+    c1->SaveAs("ntuple.pdf");
+    hgenparticle_rapidity->Draw();
+    c1->SaveAs("ntuple.pdf)");
+    c1->Close();
+    c2->cd();
     hak4pfjets_track->Draw();
-    c1->SaveAs("ntuple_ak4pfjets.pdf(");
+    c2->SaveAs("ntuple_ak4pfjets.pdf(");
     hak4pfjets_charge->Draw();
-    c1->SaveAs("ntuple_ak4pfjets.pdf");
+    c2->SaveAs("ntuple_ak4pfjets.pdf");
     hak4pfjets_pdgId->Draw();
-    c1->SaveAs("ntuple_ak4pfjets.pdf");
+    c2->SaveAs("ntuple_ak4pfjets.pdf");
     hak4pfjets_px->Draw();
-    c1->SaveAs("ntuple_ak4pfjets.pdf");
+    c2->SaveAs("ntuple_ak4pfjets.pdf");
     hak4pfjets_py->Draw();
-    c1->SaveAs("ntuple_ak4pfjets.pdf");
+    c2->SaveAs("ntuple_ak4pfjets.pdf");
     hak4pfjets_pz->Draw();
-    c1->SaveAs("ntuple_ak4pfjets.pdf");
+    c2->SaveAs("ntuple_ak4pfjets.pdf");
     hak4pfjets_pt->Draw();
-    c1->SaveAs("ntuple_ak4pfjets.pdf");
+    c2->SaveAs("ntuple_ak4pfjets.pdf");
     hak4pfjets_mass->Draw();
-    c1->SaveAs("ntuple_ak4pfjets.pdf");
+    c2->SaveAs("ntuple_ak4pfjets.pdf");
     hak4pfjets_mt->Draw();
-    c1->SaveAs("ntuple_ak4pfjets.pdf");
+    c2->SaveAs("ntuple_ak4pfjets.pdf");
     hak4pfjets_phi->Draw();
-    c1->SaveAs("ntuple_ak4pfjets.pdf");
+    c2->SaveAs("ntuple_ak4pfjets.pdf");
     hak4pfjets_theta->Draw();
-    c1->SaveAs("ntuple_ak4pfjets.pdf");
+    c2->SaveAs("ntuple_ak4pfjets.pdf");
     hak4pfjets_eta->Draw();
-    c1->SaveAs("ntuple_ak4pfjets.pdf");
+    c2->SaveAs("ntuple_ak4pfjets.pdf");
     hak4pfjets_rapidity->Draw();
-    c1->SaveAs("ntuple_ak4pfjets.pdf)");
+    c2->SaveAs("ntuple_ak4pfjets.pdf)");
+    c1->Close();
 }
 
 
